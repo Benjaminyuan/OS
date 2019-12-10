095710cf2dd5c0383430513c0304394a8fbf71ae
Benjaminyuan <1459477412@qq.com>
2019-12-10 19:34:23 +0800

    finish thread_1

--- thread/src/thread.cpp
+++ thread/src/thread.cpp
@@ -27,39 +27,42 @@ void V(int semid, int index)
 void *subp1(void *arg)
 {
 
+    int semid = *((int *)arg);
    while(1)
     {
         cout<<"thead-1 wait p "<<endl;
-        P(*((int *)arg), 0);
+        P(semid, 0);
         cout<<"thead-1 get p "<<endl;
         cout<<"a "<< a<< endl;
-        V(*((int *)arg), 0);
+        V(semid, 0);
+        if(a == 5050){
+            break;
+        }
     }
     pthread_exit(NULL);
 }
 void *subp2(void *arg)
 {
+    int semid = *((int *)arg);
     for (int i = 1; i <= 100; i++)
     {
         cout<<"thead-2 wait p "<<endl;
-        P(*((int *)arg), 0);
+        P(semid, 0);
         cout<<"thead-2 get p "<<endl;
         a += i;
-        V(*((int *)arg), 0);
+        V(semid, 0);
     }
     pthread_exit(NULL);
 }
 int main()
 {
-    cout << " 初始化semid " << endl;
     int semid = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
     pthread_t t1;
     pthread_t t2;
     // 初值为1;
-    semun arg;
-    arg.val = 1;
+
     cout << " 初始化semid " << endl;
-    semctl(semid, 0, SETVAL, arg);
+    semctl(semid, 0, SETVAL, 1);
     cout << " 初始化semid 完成" << endl;
     pthread_create(&t2, NULL, subp2, (void *)&semid);
     pthread_create(&t1, NULL, subp1, (void *)&semid);
