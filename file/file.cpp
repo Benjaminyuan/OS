a68fb2c997d898b7132bc72a3f9e5ad0811c1a1c
Benjaminyuan <1459477412@qq.com>
2019-12-23 23:24:49 +0800

    finish file

--- file/file.cpp
+++ file/file.cpp
@@ -1,27 +1,65 @@
-#include<sys/types.h>
-#include<stdio.h>
-#include<dirent.h>
-#include<sys/stat.h>
-#include<stdlib.h>
-#include<unistd.h>
-const char fileName[100]= "/Users/mac/Documents/ComputerSystem/operatingSystem/OS";
-int main(){
-    DIR* dir = opendir(fileName);
-    if(dir == NULL){
+#include <sys/types.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+void print_dir(const char *file_name, int depth)
+{
+    DIR *dir = opendir(file_name);
+    if (dir == NULL)
+    {
         perror("dir not exist");
         exit(0);
     }
-    while(1){
-        dirent* subDir = readdir(dir);
-        if (subDir == NULL){
-            break;
+    chdir(file_name);
+    dirent *sub_dir;
+    struct stat buf;
+    while ((sub_dir = readdir(dir)) != NULL)
+    {
+        lstat(sub_dir->d_name, &buf);
+        if (S_ISDIR(buf.st_mode))
+        {
+            if (strcmp(sub_dir->d_name, "..") == 0 || strcmp(sub_dir->d_name, ".") == 0)
+            {
+                continue;
+            }
+            for (int i = 0; i < depth-1; i++)
+            {
+                if(i == 0 ){
+                    printf("|");
+                }
+                printf("\t");
+            }
+            printf("|-");
+            printf("dir:%s,depth:%d\n", sub_dir->d_name, depth);
+            if (depth >= 1000)
+            {
+                return;
+            }
+            print_dir(sub_dir->d_name, depth + 1);
         }
-        printf("---------\n");
-        printf("dir:%s\n",subDir->d_name);
-        struct stat buf;
-        stat(subDir->d_name,&buf);
-        printf("%d\n",(buf.st_mode & S_IFDIR) == S_IFDIR);
-        printf("---------\n");
+        else
+        {
+            for (int i = 0; i < depth-1; i++)
+            {
+                if(i == 0 ){
+                    printf("|");
+                }
+                printf("\t");
+            }
+            printf("|-");
+            printf("file:%s,depth:%d\n", sub_dir->d_name, depth);
+        }
     }
+    chdir("..");
+    closedir(dir);
+    return;
+}
+const char *FILENAME = "/root/OS";
+int main()
+{
+    print_dir(FILENAME, 0);
     return 0;
 }
